d5e01e46ec76a8c5d861e10c8128ba19e1c823ca
Jan Chren <dev.rindeal@outlook.com>
2016-01-17 18:28:20 +0100

    fix gh-pages build

--- gh-pages_deploy.sh
+++ gh-pages_deploy.sh
@@ -1,65 +1,116 @@
 #!/bin/bash
 
-repo_dir="$(mktemp -d)"
-src_dir="${TRAVIS_BUILD_DIR}/lib"
-docs_base_dir="${repo_dir}/docs/api/"
-apigen_base_diir="${docs_base_dir}/apigen"
-apigen_base_dir="${docs_base_dir}/phpdoc"
+[[ -z "$TRAVIS" ]] && { echo "This is a build script for Travis CI only"; exit 1; }
+
+required_php_ver="5.6"
+
+[[ "$TRAVIS_PHP_VERSION" != "$required_php_ver" ]] && {
+    echo "PHP version is ${TRAVIS_PHP_VERSION}, ${required_php_ver} required"
+    exit 0
+}
+[[ "$TRAVIS_TAG" == "" ]] && { echo "Git tag is empty"; exit 0; }
+[[ "$TRAVIS_PULL_REQUEST" != "false" ]] && { echo "Building pull request"; exit 0; }
+
+set -u
+
+repo_dir="${TRAVIS_BUILD_DIR}"
+gh_pages_dir="$( mktemp -d )"
+docs_base_dir="${gh_pages_dir}/docs/api"
+apigen_base_dir="${docs_base_dir}/apigen"
+phpdoc_base_dir="${docs_base_dir}/phpdoc"
 docs_revision="$TRAVIS_TAG"
+docs_gen_src_dir="${repo_dir}/lib"
+docs_gen_ignore_dirs=(
+    "Client/HttpClient/Wsdl/*"
+)
 
-git config --global user.name "Travis CI"
-git config --global user.email "travis-ci@example.com"
+join_array () {
+    local IFS="$1"
+    shift
+    echo "$*"
+}
 
-git clone --depth 1 https://github.com/${TRAVIS_REPO_SLUG}.git --branch gh-pages --single-branch "$repo_dir" >/dev/null 2>&1 || exit 1
-cd "$repo_dir"
+
+git clone --depth 1 --branch gh-pages --single-branch \
+    https://github.com/${TRAVIS_REPO_SLUG}.git "$gh_pages_dir" \
+    >/dev/null 2>&1 || exit 1
+cd "$gh_pages_dir"
+rm -rf "./.git"
+git init
+git checkout -b gh-pages
+
+cd /tmp
 
 ## ApiGen
 
+echo "--------------------------------------------------------------------------------"
+echo "------------------------------       ApiGen       ------------------------------"
+echo "--------------------------------------------------------------------------------"
+
 apigen_dir="${apigen_base_dir}/${docs_revision}"
-apigen="$(mktemp)"
-apigen_cmd=(php "${apigen}")
-apigen_args=(
+apigen="$( mktemp )"
+apigen_cmd=(
+    php "${apigen}"
     generate
-    -s "$src_dir"
-    -d "$docs_dir"
+    --source "$docs_gen_src_dir"
+    --destination "$apigen_dir"
     --template-theme=bootstrap
     --debug
+    --tree
+    --exclude "$( join_array , "${docs_gen_ignore_dirs[@]}" )"
 )
 
-wget https://github.com/ApiGen/ApiGen/releases/download/v4.1.2/apigen.phar -O "$apigen"
+wget https://github.com/ApiGen/ApiGen/releases/download/v4.1.2/apigen.phar -O "$apigen" || exit 1
 
 rm -rf "$apigen_dir" || true
 mkdir -p "$apigen_dir"
-"${apigen_cmd[@]}" "${apigen_args[@]}" || exit 2
-ln -f -s "$apigen_dir" "${apigen_base_dir}/latest"
+
+echo "Running ApiGen: " "${apigen_cmd[@]}"
+"${apigen_cmd[@]}" || exit 11
+
+ln -f -s "$( basename "$apigen_dir" )" "${apigen_base_dir}/latest"
 
 ## phpDocumentor
 
+echo "--------------------------------------------------------------------------------"
+echo "------------------------------    phpDocumentor   ------------------------------"
+echo "--------------------------------------------------------------------------------"
+
 phpdoc_dir="${phpdoc_base_dir}/${docs_revision}"
-phpdoc_cache_dir="~/phpdoc_cache"
-phpdoc="$(mktemp)"
-phpdoc_cmd=(php "${phpdoc}")
-phpdoc_args=(
+phpdoc="$( mktemp )"
+phpdoc_cmd=(
+    travis_wait 30 # https://docs.travis-ci.com/user/build-timeouts/#Build-times-out-because-no-output-was-received
+    php "${phpdoc}" 
     run
-    -d "$src_dir"
-    -t "$docs_dir"
-    --cache-folder "$phpdoc_cache_dir"
-    --verbose
+    --directory "$docs_gen_src_dir"
+    --target "$phpdoc_dir"
     --no-interaction
     --visibility "public,protected"
     --sourcecode
-    --no-ansi
+    --cache-folder "/tmp/phpdoc_cache"
+    --ignore "$( join_array , "${docs_gen_ignore_dirs[@]}" )"
 )
 
-wget https://github.com/phpDocumentor/phpDocumentor2/releases/download/v2.8.5/phpDocumentor.phar -O "$phpdoc"
+wget https://github.com/phpDocumentor/phpDocumentor2/releases/download/v2.8.5/phpDocumentor.phar -O "$phpdoc" || exit 1
 
 rm -rf "$phpdoc_dir" || true
 mkdir -p "$phpdoc_dir"
-"${phpdoc_cmd[@]}" "${phpdoc_args[@]}" || exit 3
-ln -f -s "$phpdoc_dir" "${phpdoc_base_dir}/latest"
+
+echo "Running phpDocumentor: " "${phpdoc_cmd[@]}"
+"${phpdoc_cmd[@]}" || exit 22
+
+ln -f -s "$( basename "$phpdoc_dir" )" "${phpdoc_base_dir}/latest"
 
 ## Push
 
+cd "$gh_pages_dir"
+
 git add .
-git commit -m "API docs regenerated for tag ${TRAVIS_TAG}"
-git push "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}" gh-pages:gh-pages >/dev/null 2>&1 || exit 126
+git commit -m "API docs regenerated - ${TRAVIS_TAG}
+
+tag: ${TRAVIS_TAG}
+build number: ${TRAVIS_JOB_NUMBER}
+build id: ${TRAVIS_JOB_ID}
+"
+git push -f "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}" gh-pages:gh-pages >/dev/null 2>&1 || exit 126
+
